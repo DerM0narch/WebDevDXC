44ad920e31358eb788d5dd43e68a3b6f965a258b
Hoff1GGOD <lars.hofmann0902@outlook.com>
2021-03-02 16:29:09 +0100

    Neue Abfrage

---
+++ DatenBank/Spieler_Infos_Anzeigen.sql
@@ -0,0 +1,5 @@
+SELECT p.p_name AS 'Surname', p.p_vname AS 'Name', p.p_ingamename AS 'Summonername', t.te_name AS 'Team', r.r_bezeichnung AS 'Role' FROM t_player AS p
+JOIN t_rolle AS r ON r.r_id=p.p_r_rolle
+JOIN t_team AS t ON t.te_id=p.p_team
+
+WHERE p.p_id=1
