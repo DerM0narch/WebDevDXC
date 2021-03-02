3ab29fafccf1ae4a26adb149924ea13fe34dee27
DerM0narch <p.lokau@web.de>
2021-03-02 16:02:10 +0100

    navbar

--- index.php
+++ index.php
@@ -6,10 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>League of Legends Tournament</title>
         <link rel="stylesheet" href="css/style.css">
+        <?php include 'navigation.php' ?>
     </head>
     <body>
-     Hello
-     <img src="Pictures\sword.png" alt="Kills" width="30" height="30">
      <button type="submit" class="button"><span>TEST </span></button>
         <script src="js/main.js"></script>
     </body>
