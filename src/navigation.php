f087944875fc722dd785e377e8684c4f5a57304a
DerMonarch <p.lokau@web.de>
2021-03-03 20:22:29 +0100

    Navbar to Sidebar | need some fixes

--- src/navigation.php
+++ src/navigation.php
@@ -1,26 +1,38 @@
 <head>
   <link rel="stylesheet" href="css/style.css">
   <script src="https://kit.fontawesome.com/763c726486.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script>
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "200px";
+      document.getElementById("main").style.marginLeft = "200px";
+    }
+
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "40px";
+      document.getElementById("main").style.marginLeft = "40px";
+    }
+  </script>
 </head>
 
-<div class="topnav">
+<div id="mySidenav" class="sidenav" onmouseover="openNav()" onmouseout="closeNav()">
   <a <?php if (stripos($_SERVER['REQUEST_URI'], 'index.php') !== false) {
         echo 'class="active"';
-      } ?> href="index.php"><i class="fas fa-home"></i> Home</a>
+      } ?> href="index.php"><span class="material-icons sidespace">home</span>Home</a>
   <a <?php if (stripos($_SERVER['REQUEST_URI'], 'teams.php') !== false) {
         echo 'class="active"';
-      } ?> href="teams.php"><i class="fas fa-users"></i> Teams</a>
+      } ?> href="teams.php"><span class="material-icons sidespace">groups</span>Teams</a>
   <a <?php if (stripos($_SERVER['REQUEST_URI'], 'players.php') !== false) {
         echo 'class="active"';
-      } ?> href="players.php"><i class="fas fa-user"></i> Spieler</a>
+      } ?> href="players.php"><span class="material-icons sidespace">person</span>Spieler</a>
   <a <?php if (stripos($_SERVER['REQUEST_URI'], 'matches.php') !== false) {
         echo 'class="active"';
-      } ?> href="matches.php"><i class="fas fa-chess"></i> Matches</a>
+      } ?> href="matches.php"><span class="material-icons sidespace">sports_esports</span>Matches</a>
   <a <?php if (stripos($_SERVER['REQUEST_URI'], 'login.php') !== false) {
         echo 'class="active"';
-      } ?> href="login.php" style="float:right; margin-right: 0;"><i class="fas fa-sign-in-alt"></i> Login</a>
-</div> <br>
+      } ?> href="login.php" style="margin-top: 50px;"><span class="material-icons sidespace">login</span>Login</a>
+</div>
 
-<footer style="font-size: xx-small; position: fixed; bottom: 0;">
+<footer style=" font-size: xx-small; position: fixed; bottom: 0;">
   © Hilal, Lars, Patrick 2021
 </footer>
