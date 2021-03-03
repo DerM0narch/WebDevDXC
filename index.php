b6d397a5f9ee56e73b551a52877d177f64559c5e
DerMonarch <p.lokau@web.de>
2021-03-03 14:39:08 +0100

    index change | TODO added for mysql in index

--- index.php
+++ index.php
@@ -11,12 +11,19 @@
 </head>
 
 <body style="text-align: center">
-    <!-- TODO create an Index Page -->
     <img src="pictures/lol_championship.png" alt="LoL-Icon" style="display: block; margin-left: auto; margin-right: auto; margin-top: 10px;" height="200px" width="auto">
-    <br>
-    <h3> Deine Seite mit allen wichtigen Infos</h3>
-    <h3> über deine Teams und die Spiele </h3>
-
+    <h3>Der Tournament Tracker ihres Vertrauens</h3> <br>
+    <div class="row">
+        <div class="col">
+            <i class="fas fa-clock"></i> <b style="font-size: large;"> Nächstes Spiel</b>
+            <!-- TODO Select Befehl für nächstes Spiel -->
+        </div>
+        <div class="col">
+            <i class="fas fa-trophy"></i> <b style="font-size: large;"> Top 3</b>
+            <!-- TODO Select Befehl für die Top 3 Teams bei Siegen 
+            select * from t_gesamt_stats order by s_win desc limit 3 -->
+        </div>
+    </div>
 
 
     <script src="js/main.js"></script>
