344cc9a6178b0dbde6612fc8d7aed451afcf93a8
DerMonarch <p.lokau@web.de>
2021-03-04 09:12:38 +0100

    sql preparations

--- index.php
+++ index.php
@@ -24,8 +24,31 @@
             <div class="col">
                 <i class="fas fa-trophy"></i> <b style="font-size: large;"> Top 3</b>
                 <hr>
-                <!-- TODO Select Befehl für die Top 3 Teams bei Siegen 
-            select * from t_gesamt_stats order by s_win desc limit 3 -->
+                <!-- TODO Select Befehl für die Top 3 Teams bei Siegen -->
+                <?php
+                $sql = "SELECT * FROM t_gesamt_stats ORDER BY s_win DESC LIMIT 3";
+                if ($result = mysqli_query($conn, $sql)) {
+                    if (mysqli_num_rows($result) > 0) {
+                        echo "<table>";
+                        echo "<tr>";
+                        echo "<th>TEAM</th>";
+                        echo "<th>SIEGE</th>";
+                        echo "</tr>";
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "<tr>";
+                            echo "<td>" . $row['te_name'] . "</td>";
+                            echo "<td>" . $row['s_win'] . "</td>";
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+                        mysqli_free_result($result);
+                    } else {
+                        echo "Es wurden keine Ergebnisse gefunden!";
+                    }
+                } else {
+                    echo "ERROR: $sql. " . mysqli_error($link);
+                }
+                ?>
             </div>
         </div>
     </div>
