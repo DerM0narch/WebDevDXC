3e5a5c69f2f0e882f733afd40e3bc9d8c45a514d
DerMonarch <p.lokau@web.de>
2021-03-04 10:18:35 +0100

    eh

--- register.php
+++ register.php
@@ -19,53 +19,49 @@
                 <center>
                     <!-- TODO insert into users (e-mail, username, passwort)
     check if passwort und wiederholtes Passwort gleich sonst wiederholung-->
-    <?php
+                    <?php
 
-        if(isset($_POST['submit']))
-        {
-            $u_email = $_POST['email'];
-            $u_username = $_POST['reg_benutzername'];
-            $u_passwort = $_POST['reg_passwort'];
+                    if (isset($_POST['submit'])) {
+                        $u_email = $_POST['email'];
+                        $u_username = $_POST['reg_benutzername'];
+                        $u_passwort = $_POST['reg_passwort'];
 
-            $insert = mysqli_query($conn,"INSERT INTO t_user (u_email, u_username, u_passwort) VALUES ('$u_email','$u_username', '$u_passwort')");
+                        $insert = mysqli_query($conn, "INSERT INTO t_user (u_email, u_username, u_passwort) VALUES ('$u_email','$u_username', '$u_passwort')");
 
-            if(!$insert)
-            {
-                echo mysqli_error();
-            }
-            else
-            {
-                header("Location: reg.bestätigung.php");
-            }
-        }
+                        if (!$insert) {
+                            echo mysqli_error();
+                        } else {
+                            header("Location: reg_bestätigung.php");
+                        }
+                    }
 
-        mysqli_close($conn); // Close connection
-        ?>
-    <form method="POST">
+                    mysqli_close($conn); // Close connection
+                    ?>
+                    <form method="POST">
 
-        <table class="zentrierterTabelle">
-            <tr>
-                <td>E-mail</td>
-                <td><input type="email" name="email" placeholder="bitte Mailadresse eintragen"></td>
-            </tr>
-            <tr>
-                <td>Benutername</td>
-                <td><input type="text" name="reg_benutzername" placeholder="Benutzername"></td>
-            </tr>
-            <tr>
-                <td>Passwort</td>
-                <td><input type="password" name="reg_passwort" placeholder="********"></td>
-            </tr>
-            <tr>
-                <td>Passwort wiederholen</td>
-                <td><input type="password" name="reg.wie.passwort" placeholder="********"></td>
-            </tr>
-        </table>
-        <center>
-            <button type="submit" name="submit" value="submit"> Registrieren </button>
-        <center>
-    </form>
-    <br>
+                        <table class="zentrierterTabelle">
+                            <tr>
+                                <td>E-mail</td>
+                                <td><input type="email" name="email" placeholder="bitte Mailadresse eintragen"></td>
+                            </tr>
+                            <tr>
+                                <td>Benutername</td>
+                                <td><input type="text" name="reg_benutzername" placeholder="Benutzername"></td>
+                            </tr>
+                            <tr>
+                                <td>Passwort</td>
+                                <td><input type="password" name="reg_passwort" placeholder="********"></td>
+                            </tr>
+                            <tr>
+                                <td>Passwort wiederholen</td>
+                                <td><input type="password" name="reg.wie.passwort" placeholder="********"></td>
+                            </tr>
+                        </table>
+                        <center>
+                            <button type="submit" name="submit" value="submit"> Registrieren </button>
+                            <center>
+                    </form>
+                    <br>
 </body>
 
 </html>
