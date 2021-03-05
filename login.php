3d7c03707155f3f1a23a9af5cee6080b6805684a
Hoff1GGOD <lars.hofmann0902@outlook.com>
2021-03-05 14:09:36 +0100

    .

--- login.php
+++ login.php
@@ -12,35 +12,59 @@
 </head>
 
 <body>
+    <?php 
+    //session_start();
+       
+   // if(isset($_GET['Login'])) {
+     //   $Benutzername = $_POST['Benutzername'];
+       // $passwort = $_POST['password'];
+        
+    //    $statement = $pdo->prepare("SELECT * FROM t_user WHERE email = :email");
+     //   $result = $statement->execute(array('email' => $email));
+     //   $user = $statement->fetch();
+            
+        //Überprüfung des Passworts
+       // if ($user !== false && password_verify($passwort, $user['passwort'])) {
+         //   $_SESSION['userid'] = $user['id'];
+          //  die('Login erfolgreich. Weiter zu <a href="geheim.php">internen Bereich</a>');
+        // } else {
+           // $errorMessage = "E-Mail oder Passwort war ungültig<br>";
+        //}
+        
+    //}
+    ?>
     <div class="sideways">
         <br>
         <div class="divbox">
             <h3 class="zentrierterText"> Login </h3>
             <br>
-            <table class="zentrierterTabelle">
-                <tr>
-                    <td>Benutzername:</td>
-                    <td> <input type="text" name="Benutzername" class="zentrierterText"> </td>
-                </tr>
-                <tr>
-                    <td>Kennwort:</td>
-                    <td><Input type="password" name="password" class="zentrierterText"></td>
-                </tr>
-            </table>
-            <br>
-            <center>
-                <button class="bigbutton" type="submit" onclick="window.location.href='index.php'"><span>Login </span></button>
+            <form method="POST">
+                <table class="zentrierterTabelle">
+                    <tr>
+                        <td>Benutzername:</td>
+                        <td> <input type="text" name="Benutzername" class="zentrierterText"> </td>
+                    </tr>
+                    <tr>
+                        <td>Kennwort:</td>
+                        <td><Input type="password" name="password" class="zentrierterText"></td>
+                    </tr>
+                </table>
                 <br>
+                    <center>
+                        <button class="bigbutton" type="submit" onclick="window.location.href='index.php'"><span>Login </span></button>
+                        <br>
+                    <center>
+            </form>
+                        <br>
+                <center>
+                    Noch nicht registriert? <br>
+                    Dann melden Sie sich schnell an! <br>
+                </center>
                 <center>
                     <br>
-                    <center>
-                        Noch nicht registriert? <br>
-                        Dann melden Sie sich schnell an! <br>
-                    </center>
-                    <center>
-                        <br>
-                        <button type="submit" onclick="window.location.href='register.php'"> Registrierung </button>
-                        <center>
+                    <button type="submit" onclick="window.location.href='register.php'"> Registrierung </button>
+                <center>
+            
         </div>
 
     </div>
