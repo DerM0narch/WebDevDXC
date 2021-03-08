535bcf16ce7d6f142c5aa3272cc9243765bfe853
Hoff1GGOD <lars.hofmann0902@outlook.com>
2021-03-08 14:02:02 +0100

    game_stats hinzufügen

---
+++ game_stats.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>League of Legends Tournament</title>
+    <link rel="stylesheet" href="css/style.css">
+    <?php include 'src/navigation.php' ?>
+    <?php include 'src/db_conn.php' ?>
+</head>
+
+<body>
+    <div class="sideways">
+      
+    
+
+
+
+
+    
+    </div>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
