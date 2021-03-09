1f4d9774ae155c72bbebc278c9646512b17f98ee
Patrick <p.lokau@web.de>
2021-03-09 08:35:04 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-function countup(ereignis, side)
-{
-    var alterWert;
-    var feld;
-    	
-    feld="E"+ereignis+"S"+side;
-    alterWert=parseInt(document.getElementById(feld).value);
-    document.getElementById(feld).value=alterWert+1;
+function countup(ereignis, side) {
+  var alterWert;
+  var feld;
+
+  feld = "E" + ereignis + "S" + side;
+  alterWert = parseInt(document.getElementById(feld).value);
+  document.getElementById(feld).value = alterWert + 1;
 }
 
-function countdown(ereignis, side)
-{
-    var alterWert;
-    var feld;
-    	
-    feld="E"+ereignis+"S"+side;
-    alterWert=parseInt(document.getElementById(feld).value);
-    document.getElementById(feld).value=alterWert-1;
+function countdown(ereignis, side) {
+  var alterWert;
+  var feld;
+
+  feld = "E" + ereignis + "S" + side;
+  alterWert = parseInt(document.getElementById(feld).value);
+  if (alterWert > 0) {
+    document.getElementById(feld).value = alterWert - 1;
+  }
 }
