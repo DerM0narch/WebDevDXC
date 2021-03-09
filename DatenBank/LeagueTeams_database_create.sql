eb10baecc488c4cb1219996c113b3ccfee6f9e16
Hilalyy <Hilaly98@yahoo.com>
2021-03-09 12:42:53 +0100

    :c

--- DatenBank/LeagueTeams_database_create.sql
+++ DatenBank/LeagueTeams_database_create.sql
@@ -220,57 +220,65 @@ INSERT INTO t_player (p_name, p_vname, p_ingamename, p_herkunft, p_team, p_r_rol
 ('Lexikov', 'Alexander', 'GMB Lekcycc', 'Russland', 10, 5);
 
 #Spiele
-INSERT INTO
-  t_spieltag (sp_tag, sp_blue_side, sp_red_side)
-VALUES
+
+INSERT INTO t_spieltag (sp_tag, sp_blue_side, sp_red_side) VALUES
+
   #Spieltag 1
   (1, 1, 10),
   (1, 2, 9),
   (1, 3, 8),
   (1, 4, 7),
   (1, 5, 6),
+  
   #Spieltag 2
   (2, 10, 2),
   (2, 3, 1),
   (2, 4, 9),
   (2, 5, 8),
   (2, 6, 7),
+  
   #Spieltag 3
   (3, 3, 10),
   (3, 4, 2),
   (3, 5, 1),
   (3, 6, 9),
   (3, 7, 8),
+  
   #Spieltag 4
   (4, 10, 4),
   (4, 5, 3),
   (4, 6, 2),
   (4, 7, 1),
   (4, 8, 9),
+  
   #Spieltag 5
   (5, 5, 10),
   (5, 6, 4),
   (5, 7, 3),
   (5, 8, 2),
   (5, 9, 1),
+  
   #Spieltag 6
   (6, 10, 6),
   (6, 7, 5),
   (6, 8, 4),
   (6, 9, 3),
   (6, 1, 2),
+  
   #Spieltag 7
   (7, 7, 10),
   (7, 8, 6),
   (7, 9, 5),
   (7, 1, 4),
   (7, 2, 3),
+  
   #Spieltag 8
   (8, 10, 8),
   (8, 9, 7),
   (8, 1, 6),
   (8, 2, 5),
   (8, 3, 4),
+  
   #Spieltag 9
   (9, 9, 10),
   (9, 1, 8),
