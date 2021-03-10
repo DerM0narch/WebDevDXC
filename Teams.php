a362e5d71005864f40f50e62020642319e34bf3c
Hilalyy <Hilaly98@yahoo.com>
2021-03-10 12:04:48 +0100

    --

--- Teams.php
+++ Teams.php
@@ -27,6 +27,8 @@
     </style>
 </head>
 
+
+
 <body>
     <!-- TODO #7 Select Befehl für Teams schreiben und Seite designen -->
     <div class="sideways">
@@ -37,15 +39,23 @@
             </div>
             <div class="gallery-cell"><img src="pictures/Team_Logos/Fnatic_logo.png" alt="Fnatic">
                 <hr>
+            <!-- Spieler Bilder-->
             </div>
             <div class="gallery-cell"><img src="pictures/Team_Logos/FPX_logo.png" alt="FPX">
                 <hr>
+            <!-- Spieler Bilder-->
             </div>
             <div class="gallery-cell"><img src="pictures/Team_Logos/G2_logo.png" alt="G2">
                 <hr>
+            <!-- Spieler Bilder-->
+            <div class="container"><img src="pictures\Teams_und_Spieler\G2\G2_Wunder_Top.png" alt="Avatar" class="image">
+                <div class="overlay">G2 Wunder</div>
+               
+            </div>
             </div>
             <div class="gallery-cell"><img src="pictures/Team_Logos/Gambit_logo.png" alt="Gambit">
                 <hr>
+            <!-- Spieler Bilder-->
             </div>
             <?php include 'src/teams_select.php' ?>
         </div>
@@ -53,4 +63,52 @@
     <script src="js/flickity.pkgd.min.js"></script>
 </body>
 
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box;}
+
+.container {
+  position: relative;
+  width: 50%;
+  max-width: 300px;
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.overlay {
+  position: absolute; 
+  bottom: 0; 
+  background: rgb(0, 0, 0);
+  background: rgba(0, 0, 0, 0.5); /* Black see-through */
+  color: #f1f1f1; 
+  width: 100%;
+  transition: .5s ease;
+  opacity:0;
+  color: white;
+  font-size: 20px;
+  padding: 20px;
+  text-align: center;
+}
+
+.container:hover .overlay {
+  opacity: 1;
+}
+</style>
+</head>
+<body>
+
+<h2>Image Overlay Title</h2>
+<p>Hover over the image to see the effect.</p>
+
+<div class="container">
+  <img src="pictures\Teams_und_Spieler\G2\G2_Caps_Mid.png" alt="Avatar" class="image">
+  <div class="overlay">G2 Caps</div>
+</div>
+
+</body>
 </html>
