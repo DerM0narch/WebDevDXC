c66876f94629f9f2568ae79019a44305575e08d5
Hoff1GGOD <lars.hofmann0902@outlook.com>
2021-03-11 11:30:23 +0100

    .

--- game_stats.php
+++ game_stats.php
@@ -10,69 +10,23 @@
     <?php include 'src/navigation.php' ?>
     <?php include 'src/db_conn.php' ?>
     <script src="js/main.js"></script>
-    <?php
-        if(isset($_POST['submit']))
-            {   
-                if ($_GET['type']==3)
-                {   
-                    $sp_id=$_POST['hd_spiel_id'];
-                    $bluekills=$_POST['bluekills'];
-                    $bluedrakes=$_POST['bluedrakes'];
-                    $bluenashes=$_POST['bluenashes'];
-                    $blueturret=$_POST['blueturret'];
-                    $blueinhib=$_POST['blueinhib'];
-                    $redkills=$_POST['redkills'];
-                    $reddrakes=$_POST['reddrakes'];
-                    $rednashes=$_POST['rednashes'];
-                    $redturret=$_POST['redturret'];
-                    $redinhib=$_POST['redinhib'];
 
-                    $sql = "INSERT INTO t_spiel_stats (ss_sp_id, ss_blue_kills, ss_blue_drakes, ss_blue_nashes, ss_blue_turrets, ss_blue_inhibs,
-                            ss_red_kills, ss_red_drakes, ss_red_nashes, ss_red_turrets, ss_red_inhibs ) VALUES ('$sp_id', '$bluekills', '$bluedrakes', '$bluenashes',
-                            '$blueturret', '$blueinhib', '$redkills', '$reddrakes', '$rednashes', '$redturret', '$redinhib')";
-                    
-                    echo $sql;
-
-                    //$insert=mysqli_query($conn, $sql);  
-                    
-                    
-
-                    // if (!$insert)
-                    // {
-                    //     echo "Fehlerhafte eingabe";
-                    // }
-                    // else
-                    // {
-                    //     echo "Eingabe erfolgreich";
-                    // }
-                    
-                }
-            }   
-    ?>
 </head>
 
-    <body>
-        <div class="sideways">
-            <!-- Dropdownliste zur Auswahl der Spiele
+<body>
+    <div class="sideways">
+        <!-- Dropdownliste zur Auswahl der Spiele
             in den Zeilen die Ereignissymbole plus Schaltflächen zum hoch und runter zählen für beide Teams
             button zur auswahl des Siegers je nach team färbt er sich
             Submit button-->
-            <form name="spielauswahl" id="spielauswahl" method="POST">
+            <form name="spielauswahl" id="spielauswahl" action="#" method="POST">
                 <table class="drop">
                     <tr>
                         <th>
-                        
-                            <input type="hidden" name="hd_spiel_id" id="hd_spiel_id" value=0> 
-                                <label>Spielauswahl: 
-                                <?php if(isset($_GET['type']) && ($_GET['type']==3 || $_GET['type']==2))
-                                { ?>
-                                    <select name="nextGame" id=nextGame disabled>
-                                <?php }
-                                else
-                                { ?>
-                                    <select name="nextGame" id=nextGame  onchange="selectchange(1);" >
-                                <?php } ?>
 
+                            <input type="hidden" name="hd_spiel_id" id="hd_spiel_id" value=0>
+                                <label>Spielauswahl:
+                                    <select name="nextGame" id=nextGame onchange="selectchange();">
                                         <option value=0 disabled selected> Bitte Wählen </option>
                                         <?php include 'src/game_select.php' ?>
                                     </select>
@@ -80,8 +34,46 @@
                         </th>
                     </tr>
                 </table>
-                <br>
-                
+            <br>
+                <?php
+                    if(isset($_POST['submit']))
+                        {
+                            if ($_GET['type']==3)
+                            {
+                                $sp_id=$_POST['hd_spiel_id'];
+                                $bluekills=$_POST['bluekills'];
+                                $bluedrakes=$_POST['bluedrakes'];
+                                $bluenashes=$_POST['bluenashes'];
+                                $blueturret=$_POST['blueturret'];
+                                $blueinhib=$_POST['blueinhib'];
+                                $redkills=$_POST['redkills'];
+                                $reddrakes=$_POST['reddrakes'];
+                                $rednashes=$_POST['rednashes'];
+                                $redturret=$_POST['redturret'];
+                                $redinhib=$_POST['redinhib'];
+
+                                $sql = "INSERT INTO t_spiel_stats (ss_sp_id, ss_blue_kills, ss_blue_drakes, ss_blue_nashes, ss_blue_turrets, ss_blue_inhibs,
+                                        ss_red_kills, ss_red_drakes, ss_red_nashes, ss_red_turrets, ss_red_inhibs ) VALUES ('$sp_id', '$bluekills', '$bluedrakes', '$bluenashes',
+                                        '$blueturret', '$blueinhib', '$redkills', '$reddrakes', '$rednashes', '$redturret', '$redinhib')";
+
+                                echo $sql;
+
+                                //$insert=mysqli_query($conn, $sql);
+
+
+
+                                // if (!$insert)
+                                // {
+                                //     echo "Fehlerhafte eingabe";
+                                // }
+                                // else
+                                // {
+                                //     echo "Eingabe erfolgreich";
+                                // }
+
+                            }
+                        }
+                ?>
 
             <?php if (!isset($_GET['type']) || ($_GET['type']!=2 && $_GET['type']!=3))
             echo "Noch kein Spiel ausgewählt!";
@@ -113,17 +105,17 @@
                         <td>
                             <!-- Schaltfläschen fürs hoch/runter zählen + aktueller Stand-->
                             <input type="button" value="-" name="tbkiminus" id="tbkiminus" onclick="countdown(1,1);">
-                                
+
                                     <input type="text" name="tbki" id="E1S1" value=0 readonly>
-                                
+
                             <input type="button" value="+" name="tbkiplus" id="tbkiplus" onclick="countup(1,1);">
                         </td>
                         <td>
                             <!-- Schaltfläschen fürs hoch/runter zählen + aktueller Stand-->
                             <input type="button" value="-" name="trkiminus" id="trkiminus" onclick="countdown(1,2);">
-                                
+
                                     <input type="text" name="trki" id="E1S2" value=0 readonly>
-                                
+
                             <input type="button" value="+" name="trkiplus" id="trkiplus" onclick="countup(1,2);">
                         </td>
                         <td>
@@ -142,17 +134,17 @@
                         <td>
                             <!-- Schaltfläschen fürs hoch/runter zählen + aktueller Stand-->
                             <input type="button" value="-" name="tbtuminus" id="tbtuminus" onclick="countdown(2,1);">
-                                
+
                                     <input type="text" name="tbtu" id="E2S1" value=0 readonly>
-                                
+
                             <input type="button" value="+" name="tbtuplus" id="tbtuplus" onclick="countup(2,1);">
                         </td>
                         <td>
                             <!-- Schaltfläschen fürs hoch/runter zählen + aktueller Stand-->
                             <input type="button" value="-" name="trtuminus" id="trtuminus" onclick="countdown(2,2);">
-                                
+
                                     <input type="text" name="trtu" id="E2S2" value=0 readonly>
-                                
+
                             <input type="button" value="+" name="trtuplus" id="trtuplus" onclick="countup(2,2);">
                         </td>
                         <td>
@@ -170,17 +162,17 @@
                         <td>
                             <!-- Schaltfläschen fürs hoch/runter zählen + aktueller Stand-->
                             <input type="button" value="-" name="tbdrminus" id="tbdrminus" onclick="countdown(3,1);">
-                                
+
                                     <input type="text" name="tbdr" id="E3S1" value=0 readonly>
-                                
+
                             <input type="button" value="+" name="tbdrplus" id="tbdrplus" onclick="countup(3,1);">
                         </td>
                         <td>
                             <!-- Schaltfläschen fürs hoch/runter zählen + aktueller Stand-->
                             <input type="button" value="-" name="trdrminus" id="trdrminus" onclick="countdown(3,2);">
-                                
+
                                     <input type="text" name="trdr" id="E3S2" value=0 readonly>
-                                
+
                             <input type="button" value="+" name="trdrplus" id="trdrplus" onclick="countup(3,2);">
                         </td>
                         <td>
@@ -197,17 +189,17 @@
                         <td>
                             <!-- Schaltfläschen fürs hoch/runter zählen + aktueller Stand-->
                             <input type="button" value="-" name="tbnaminus" id="tbnaminus" onclick="countdown(4,1);">
-                                
+
                                     <input type="text" name="tbna" id="E4S1" value=0 readonly>
-                                
+
                             <input type="button" value="+" name="tbnaplus" id="tbnaplus" onclick="countup(4,1);">
                         </td>
                         <td>
                             <!-- Schaltfläschen fürs hoch/runter zählen + aktueller Stand-->
                             <input type="button" value="-" name="trnaminus" id="trnaminus" onclick="countdown(4,2);">
-                                
+
                                     <input type="text" name="trna" id="E4S2" value=0 readonly>
-                                
+
                             <input type="button" value="+" name="trnaplus" id="trnaplus" onclick="countup(4,2);">
                         </td>
                         <td>
@@ -224,17 +216,17 @@
                         <td>
                             <!-- Schaltfläschen fürs hoch/runter zählen + aktueller Stand-->
                             <input type="button" value="-" name="tbinminus" id="tbinminus" onclick="countdown(5,1);">
-                                
+
                                     <input type="text" name="tbin" id="E5S1" value=0 readonly>
-                                
+
                             <input type="button" value="+" name="tbinplus" id="tbinplus" onclick="countup(5,1);">
                         </td>
                         <td>
                             <!-- Schaltfläschen fürs hoch/runter zählen + aktueller Stand-->
                             <input type="button" value="-" name="trinminus" id="trinminus" onclick="countdown(5,2);">
-                                
+
                                     <input type="text" name="trin" id="E5S2" value=0 readonly>
-                                
+
                             <input type="button" value="+" name="trinplus" id="trinplus" onclick="countup(5,2);">
                         </td>
                         <td>
@@ -246,11 +238,11 @@
                 <br>
                 <table>
                     <tr>
-                        <input type="button" name="submit" value="einfügen" onclick="selectchange(2);"/>
+                        <input type="button" name="submit" value="einfügen" onclick="datenSpeichern();"/>
                     </tr>
                 </table>
             <?php } ?>
             </form>
-        </div>    
-    </body>        
+        </div>
+    </body>
 </html>
