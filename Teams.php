7bc6afd8abddb6f016217c8727bf933114a5876e
Hilalyy <Hilaly98@yahoo.com>
2021-03-11 15:44:34 +0100

    3

--- Teams.php
+++ Teams.php
@@ -27,11 +27,8 @@
             margin: auto;
             height: 100px;
         }
-    </style>
 
-<!--Style für die Bilder + Hovertext -->
-
-
+        </style>
 
 
 <head>
@@ -40,8 +37,13 @@
     <div class="sideways">
             <div class="gallery js-flickity">
             
-            <div class="gallery-cell"><img src="pictures/Team_Logos/G2_logo.png" alt="G2"><hr>
-                <br>     
+            <div class="gallery-cell"><img src="pictures/Team_Logos/G2_logo.png" alt="G2">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                
             <img src="pictures\Teams_und_Spieler\G2\G2_Wunder_Top.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\G2\G2_Jankos_Jungle.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\G2\G2_Caps_Mid.png" alt="Avatar" class="image">
@@ -49,8 +51,12 @@
             <img src="pictures\Teams_und_Spieler\G2\G2_Mikyx_Sup.png" alt="Avatar" class="image">
             </div>
 
-            <div class="gallery-cell"><img src="pictures/Team_Logos/Fnatic_logo.png" alt="Fnatic"><hr>
-                 <br>     
+            <div class="gallery-cell"><img src="pictures/Team_Logos/Fnatic_logo.png" alt="Fnatic">   
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
             <img src="pictures\Teams_und_Spieler\Fnatic\FNC_Bwipo_Top.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\Fnatic\FNC_Selfmade_Jungle.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\Fnatic\FNC_Nisqy_Mid.png" alt="Avatar" class="image">
@@ -58,8 +64,12 @@
             <img src="pictures\Teams_und_Spieler\Fnatic\FNC_Hylissang_Sup.png" alt="Avatar" class="image">
             </div>
 
-            <div class="gallery-cell"><img src="pictures/Team_Logos/Rogue_logo.png" alt="RGE"><hr>
-                 <br>    
+            <div class="gallery-cell"><img src="pictures/Team_Logos/Rogue_logo.png" alt="RGE">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
             <img src="pictures\Teams_und_Spieler\Rogue\RGE_Odoamne_Top.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\Rogue\RGE_Inspired_Jungle.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\Rogue\RGE_Larssen_Mid.png" alt="Avatar" class="image">
@@ -67,7 +77,11 @@
             <img src="pictures\Teams_und_Spieler\Rogue\RGE_Trymbi_Sup.png" alt="Avatar" class="image">
             </div>
 
-            <div class="gallery-cell"><img src="pictures/Team_Logos/Schalke04_logo.png" alt="S04"><hr>
+            <div class="gallery-cell"><img src="pictures/Team_Logos/Schalke04_logo.png" alt="S04">
+                <br>
+                <br>
+                <br>
+                <br>
                 <br>
             <img src="pictures\Teams_und_Spieler\Schalke04\S04_Broken_Blade_Top.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\Schalke04\S04_Gilius_Jungle.png" alt="Avatar" class="image">
@@ -76,7 +90,11 @@
             <img src="pictures\Teams_und_Spieler\Schalke04\S04_LIMIT_Sup.png" alt="Avatar" class="image">    
             </div>
 
-            <div class="gallery-cell"><img src="pictures/Team_Logos/SKGaming_logo.png" alt="SK"><hr>
+            <div class="gallery-cell"><img src="pictures/Team_Logos/SKGaming_logo.png" alt="SK">
+                <br>
+                <br>
+                <br>
+                <br>
                 <br>
             <img src="pictures\Teams_und_Spieler\SK\SK_Jenax_Top.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\SK\SK_TynX_Jungle.png" alt="Avatar" class="image">
@@ -85,7 +103,11 @@
             <img src="pictures\Teams_und_Spieler\SK\SK_Treatz_Support.png" alt="Avatar" class="image">   
             </div>
             
-            <div class="gallery-cell"><img src="pictures/Team_Logos/UOF_logo.png" alt="UOL"><hr>
+            <div class="gallery-cell"><img src="pictures/Team_Logos/UOF_logo.png" alt="UOL">
+                <br>
+                <br>
+                <br>
+                <br>
                 <br>
             <img src="pictures\Teams_und_Spieler\UOF.cis\UOL_BOSS_Top.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\UOF.cis\UOL_AHaHaCiK_Jungle.png" alt="Avatar" class="image">
@@ -94,7 +116,11 @@
             <img src="pictures\Teams_und_Spieler\UOF.cis\UOL_SaNTaS_Sup.png" alt="Avatar" class="image">     
             </div>
 
-            <div class="gallery-cell"><img src="pictures/Team_Logos/FPX_logo.png" alt="FPX"><hr>
+            <div class="gallery-cell"><img src="pictures/Team_Logos/FPX_logo.png" alt="FPX">
+                <br>
+                <br>
+                <br>
+                <br>
                 <br>
             <img src="pictures\Teams_und_Spieler\FPX\FPX_Nuguri_Top.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\FPX\FPX_Beichuan_Jungle.png" alt="Avatar" class="image">
@@ -104,7 +130,11 @@
             
             </div>
 
-            <div class="gallery-cell"><img src="pictures/Team_Logos/VEG_logo.png" alt="VEG"><hr>
+            <div class="gallery-cell"><img src="pictures/Team_Logos/VEG_logo.png" alt="VEG">
+                <br>
+                <br>
+                <br>
+                <br>
                 <br>
             <img src="pictures\Teams_und_Spieler\VEG\VEG_Charger_Top.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\VEG\VEG_Rein_Jungle.png" alt="Avatar" class="image">
@@ -113,7 +143,11 @@
             <img src="pictures\Teams_und_Spieler\VEG\VEG_Mercenary_Sup.png" alt="Avatar" class="image">   
             </div>
 
-            <div class="gallery-cell"><img src="pictures/Team_Logos/DWG_logo.png" alt="DWG"><hr>
+            <div class="gallery-cell"><img src="pictures/Team_Logos/DWG_logo.png" alt="DWG">
+                <br>
+                <br>
+                <br>
+                <br>
                 <br>
             <img src="pictures\Teams_und_Spieler\DWG\DK_Khan_Top.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\\DWG\DK_Canyon_Jungle.png" alt="Avatar" class="image">
@@ -122,7 +156,11 @@
             <img src="pictures\Teams_und_Spieler\DWG\DK_BeryL_Sup.png" alt="Avatar" class="image">   
             </div>
 
-            <div class="gallery-cell"><img src="pictures/Team_Logos/Gambit_logo.png" alt="Gambit"><hr>
+            <div class="gallery-cell"><img src="pictures/Team_Logos/Gambit_logo.png" alt="Gambit">
+                <br>
+                <br>
+                <br>
+                <br>
                 <br>
             <img src="pictures\Teams_und_Spieler\Gambit\GMB_Dreampull_Top.png" alt="Avatar" class="image">
             <img src="pictures\Teams_und_Spieler\Gambit\GMB_Four_Jungle.png" alt="Avatar" class="image">
