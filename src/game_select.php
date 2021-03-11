a12d19d04674d9895de93e598768e00cfad2488b
Hoff1GGOD <lars.hofmann0902@outlook.com>
2021-03-11 13:54:18 +0100

    .

--- src/game_select.php
+++ src/game_select.php
@@ -1,6 +1,6 @@
 <?php
 $aktspiel=$_POST['hd_spiel_id'];
-$sql = 'Select sp_id from t_spieltag order by sp_id';
+$sql = 'Select sp_id from t_spieltag where sp_blue_win is null order by sp_id';
 $res = mysqli_query($conn, $sql);
 if ($res) {
     while ($zeile = mysqli_fetch_array($res)) {
