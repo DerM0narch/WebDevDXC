1ade0a25c061a7c6c9bc5555865888860a73964f
Hoff1GGOD <lars.hofmann0902@outlook.com>
2021-03-11 15:50:27 +0100

    gamestats_perfeto

--- src/db_conn.php
+++ src/db_conn.php
@@ -1,4 +1,16 @@
 <?php
+function mysqli_result($res,$row=0,$col=0)
+{ 
+    $numrows = mysqli_num_rows($res); 
+    if ($numrows && $row <= ($numrows-1) && $row >=0){
+        mysqli_data_seek($res,$row);
+        $resrow = (is_numeric($col)) ? mysqli_fetch_row($res) : mysqli_fetch_assoc($res);
+        if (isset($resrow[$col])){
+            return $resrow[$col];
+        }
+    }
+    return false;
+}
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -10,3 +22,4 @@ if ($conn->connect_error) {
     die("<p style='float:right;'><span class='material-icons sidespace'>cloud_off</span></p> " . $conn->connect_error);
 }
 echo "<p style='float:right;'><span class='material-icons sidespace'>cloud_done</span></p>";
+
