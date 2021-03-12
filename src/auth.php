f17c4d13f2e07a08a6df86e716a207d9e940707a
Patrick <p.lokau@web.de>
2021-03-12 09:44:03 +0100

    fix

--- src/auth.php
+++ src/auth.php
@@ -1,5 +1,4 @@
 <?php
-     session_start();
 
      $hostname = $_SERVER['HTTP_HOST'];
      $path = dirname($_SERVER['PHP_SELF']);
@@ -8,4 +7,3 @@
       header('Location: http://'.$hostname.($path == '/' ? '' : $path).'/login.php');
       exit;
       }
-?>
