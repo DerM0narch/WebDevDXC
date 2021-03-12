5077e2fdb16dc74d34b332bfad90a2543d5101d1
Hoff1GGOD <lars.hofmann0902@outlook.com>
2021-03-12 08:36:27 +0100

    nextgame gefixed

--- src/index_nextgame.php
+++ src/index_nextgame.php
@@ -1,17 +1,17 @@
 <?php
 $sql = "SELECT
 (SELECT te_pic_path FROM t_team JOIN t_spieltag ON sp_blue_side = te_id WHERE sp_id=	(	SELECT sp_id FROM t_spieltag
-																												WHERE sp_blue_win IS NULL
+																												WHERE sp_win IS NULL
 																												ORDER BY sp_id
 																												LIMIT 1)) AS 'picblue',
 (SELECT te_pic_path FROM t_team JOIN t_spieltag ON sp_red_side = te_id WHERE sp_id=	(	SELECT sp_id FROM t_spieltag
-																												WHERE sp_blue_win IS NULL
+																												WHERE sp_win IS NULL
 																												ORDER BY sp_id
 																												LIMIT 1)) AS 'picred'
 FROM t_team
 JOIN t_spieltag ON sp_blue_side=te_id
 WHERE sp_id = (	SELECT sp_id FROM t_spieltag
-						WHERE sp_blue_win IS NULL
+						WHERE sp_win IS NULL
 						ORDER BY sp_id
 						LIMIT 1)";
 if ($result = mysqli_query($conn, $sql)) {
